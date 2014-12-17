4215cc6be1a65da67090d139119c028b5494f4de
Kra1o5 <kra1o5x@gmail.com>
2014-12-17 19:18:43 +0100

    Adapt to Omni

--- omni_aquarise5.mk
+++ omni_aquarise5.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/bq/aquarise5/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aquarise5
-PRODUCT_NAME := cm_aquarise5
+PRODUCT_NAME := omni_aquarise5
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris E5

7f6e3396fc5aa687c153908330c87315e43bcf00
Kra1o5 <kra1o5x@gmail.com>
2014-12-18 16:19:01 +0100

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/bq/aquarise5/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := aquarise5
+PRODUCT_NAME := cm_aquarise5
+PRODUCT_BRAND := bq
+PRODUCT_MANUFACTURER := bq
+PRODUCT_MODEL := Aquaris E5

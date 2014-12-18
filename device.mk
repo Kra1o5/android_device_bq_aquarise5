b7c18364657b3ccb33b4e6db9a015466fe100acf
Kra1o5 <kra1o5x@gmail.com>
2014-12-18 16:19:01 +0100

    Initial commit

---
+++ device.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := device/bq/aquarise5
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/kernel:kernel \
+    $(LOCAL_PATH)/dt.img:dt.img \
+    $(LOCAL_PATH)/recovery/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/recovery/init:root/init \
+    $(LOCAL_PATH)/recovery/charger:root/charger \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_0.png:root/res/images/charger/battery_0.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_1.png:root/res/images/charger/battery_1.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_2.png:root/res/images/charger/battery_2.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_3.png:root/res/images/charger/battery_3.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_4.png:root/res/images/charger/battery_4.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_5.png:root/res/images/charger/battery_5.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_6.png:root/res/images/charger/battery_6.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_charge.png:root/res/images/charger/battery_charge.png \
+    $(LOCAL_PATH)/recovery/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png \
+    $(LOCAL_PATH)/recovery/sbin/healthd:root/sbin/healthd
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := aquarise5
